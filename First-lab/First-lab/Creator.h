6b55c23178ffe5bd59dc8d44c3dca1a427a1fb1d
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-02-21 17:39:21 +0300

    feat: add function of create binary file

---
+++ First-lab/First-lab/Creator.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef CREATOR_H
+#define CREATOR_H
+
+void createBinFile(std::string fileName, std::ofstream& out);
+
+#endif
--- First-lab/First-lab/Main.cpp
+++ First-lab/First-lab/Main.cpp
@@ -1,10 +1,26 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include "Creator.h"
 
 int main() {
 	std::cout << "Input initial filename: ";
 	std::string initialFileName;
 	std::cin >> initialFileName;
+	std::cout << "Input number of records in file: ";
+	int recordsNumber;
+	std::cin >> recordsNumber;
+
+	std::ofstream out;
+	createBinFile(initialFileName, out);
+
+	if(!out.is_open()) {
+		std::cerr << "Open file failed.\n";
+		return 0;
+	} else {
+		std::cout << "File is open.\n";	
+	}
+
+	system("pause");
 	return 0;
 }
