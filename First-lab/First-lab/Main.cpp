57cd2b7920d5c284b07f58acf3bba54a5e724d74
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-02-21 16:54:29 +0300

    init: start 1st lab

---
+++ First-lab/First-lab/Main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+int main() {
+	std::cout << "Input initial filename: ";
+	std::string initialFileName;
+	std::cin >> initialFileName;
+	return 0;
+}
