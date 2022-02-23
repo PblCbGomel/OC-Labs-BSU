a2e2ab4df5fdace11aced6bcda8a763a5822e98e
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-02-23 20:57:52 +0300

    refactor: delete temp catalog

--- temp/Creator.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-
-void createBinFile(std::string fileName, std::ofstream& out) {
-	out.open(fileName.c_str(), std::ios::binary);
-}
--- temp/Creator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef CREATOR_H
-#define CREATOR_H
-
-void createBinFile(std::string fileName, std::ofstream& out);
-
-#endif
