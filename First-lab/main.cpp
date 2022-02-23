ca265cc95609174abbca64edaca00e4357801025
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-02-23 21:50:16 +0300

    feat: add reported functional

--- First-lab/main.cpp
+++ First-lab/main.cpp
@@ -28,16 +28,35 @@ bool createBinFile(std::string fileName, int count) {
 void printFromBinFile(std::string fileName) {
     std::ifstream in(fileName.c_str(), std::ios::in|std::ios::binary|std::ios::ate);
     in.seekg(0, std::ios::end);
-    int n = in.tellg()/sizeof(employee);
+    int n = in.tellg() / sizeof(employee);
     in.seekg(0, std::ios::beg);
-    employee* emps = new employee[n];
-    in.read((char*)emps, n*sizeof(employee));
+    employee* allEmployees = new employee[n];
+    in.read((char*)allEmployees, n*sizeof(employee));
     in.close();
     for(int i = 0; i < n; i++){
-        std::cout << std::setw(5) << emps[i].num
-             << std::setw(10) << emps[i].name
-             << std::setw(5)<< emps[i].hours << '\n';
+        std::cout << std::setw(5) << allEmployees[i].num
+             << std::setw(12) << allEmployees[i].name
+             << std::setw(10)<< allEmployees[i].hours << '\n';
     }
+}
+
+bool writeInReportFile(std::string inputFile, std::string reportFile, int salary){
+    STARTUPINFO si;
+    PROCESS_INFORMATION piApp;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    char sal[10];
+    itoa(salary, sal, 10);
+    std::string arg = "Reporter.exe " + inputFile + " " + reportFile + " " + std::string(sal);
+    char args[255];
+    strcpy(args, arg.c_str());
+    bool isStarted = CreateProcess(NULL, args, NULL,
+                                   NULL, FALSE,CREATE_NEW_CONSOLE,
+                                   NULL, NULL, &si, &piApp);
+    WaitForSingleObject(piApp.hProcess, INFINITE);
+    CloseHandle(piApp.hThread);
+    CloseHandle(piApp.hProcess);
+    return isStarted;
 }
 
 int main(int argc, char *argv[]) {
@@ -56,9 +75,15 @@ int main(int argc, char *argv[]) {
         system("cls");
         std::cout << "File " << initialFileName << " created.\n";
         printFromBinFile(initialFileName);
-        std::string recorderFile;
-        std::cout << "Input recorder file name: ";
-        std::cin >> recordsNumber;
+        std::string reporterFileName;
+        std::cout << "Input reporter file name: ";
+        std::cin >> reporterFileName;
+        int salary;
+        std::cout << "Input salary for 1h working: ";
+        std::cin >> salary;
+        if(writeInReportFile(initialFileName, reporterFileName, salary)){
+            std::cout << "Report file created.\n";
+        }
     }
 
     system("pause");
