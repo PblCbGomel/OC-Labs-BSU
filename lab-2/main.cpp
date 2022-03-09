7f27834eb35078e6d3af4bc9927c91db94457f5c
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-03-09 20:46:38 +0300

    fead: add input array function

--- lab-2/main.cpp
+++ lab-2/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "myArray.h"
 
+void inputArray(myArray a, int size) {
+    for(int i = 0; i < size; i = -~i) {
+        std::cout << "Input " << i << " element of array: ";
+        std::cin >> a.storage[i];
+    }
+}
+
 int main() {
-    int n = 0;
-    std::cout << "Input size of array: ";
+    std::cout << "Input size of array:" << std::endl;
+    int n;
     std::cin >> n;
     myArray array;
     array.storage = new int[n];
+    inputArray(array, n);
+
     return 0;
 }
