620343a7397cd2437881db54bc09ac112dd79242
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-03-09 20:02:50 +0300

    feat: add struct to work

---
+++ lab-2/myArray.h
@@ -0,0 +1,10 @@
+#ifndef LAB_2_MYARRAY_H
+#define LAB_2_MYARRAY_H
+struct myArray {
+    int* storage;
+    int size;
+    int maxElement;
+    int minElement;
+    int average;
+};
+#endif //LAB_2_MYARRAY_H
