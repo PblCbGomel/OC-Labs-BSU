aaeaf9e266e318eae63a8bb1e052129aa12c0d24
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-05-25 19:11:40 +0300

    feat: add client implementation

--- Lab5/employee.h
+++ Lab5/employee.h
@@ -1,6 +1,15 @@
-//
-// Created by Asus on 5/25/2022.
-//
+#include <iostream>
+
+struct employee {
+    int num;
+    char name[10];
+    double hours;
+
+    void print(){
+        std::cout << "ID: " << num << "\tName: " << name
+            << "\tHours: " << hours << '\n';
+    }
+};
 
 #ifndef LAB5_EMPLOYEE_H
 #define LAB5_EMPLOYEE_H
