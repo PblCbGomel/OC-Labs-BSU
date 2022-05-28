798416428d8e24a71b41dbf80e98a5d369cdb4ed
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-05-28 15:57:31 +0300

    feat: add server realisation

--- Lab5/employee.h
+++ Lab5/employee.h
@@ -11,6 +11,10 @@ struct employee {
     }
 };
 
+int empCmp(const void* p1, const void* p2){
+    return ((employee*)p1)->num - ((employee*)p2)->num;
+}
+
 #ifndef LAB5_EMPLOYEE_H
 #define LAB5_EMPLOYEE_H
 
