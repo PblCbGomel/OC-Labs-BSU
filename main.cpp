85625fc3e70a7abbfd63c8396ec67759c305c503
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-06-21 10:18:34 +0300

    feat: add print array function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <mutex>
+#include <vector>
+
+std::mutex markerMutex;
+
+void printArray(std::vector<int> arr){
+    std::lock_guard<std::mutex> lk(markerMutex);
+    int size = arr.size();
+    for(int i = 0; i < size; ++i){
+        std::cout << arr[i];
+    }
+    std::cout << '\n';
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
