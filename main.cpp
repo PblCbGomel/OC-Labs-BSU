0a3171c04b531dd7fcabd4fb060ae11c4469fa39
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-06-21 10:38:21 +0300

    feat: add marker

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <mutex>
 #include <vector>
+#include <condition_variable>
+#include "thread.h"
 
 std::mutex markerMutex;
+std::mutex startMutex;
 
-int getRandomNumber() {
-    return ((rand() * 10000 / 5 + 123) % 65 * 8 % 123 * 902 / 2 + 12345) % 100;
+std::condition_variable startCV;
+std::condition_variable stateChanged;
+
+bool ready;
+std::vector<bool> threadsPaused;
+
+int getRandomNumber(int arg) {
+    srand(time(NULL));
+    return ((rand() * 10000 / 5 + 123) % 65 * 8 % 123 * 902 / 2 + 12345) % arg;
 }
 
 void printArray(std::vector<int> arr){
@@ -15,6 +25,57 @@ void printArray(std::vector<int> arr){
         std::cout << arr[i];
     }
     std::cout << '\n';
+}
+
+bool allPaused(bool* arr, int n){
+    for(int i = 0; i < n; i++){
+        if(!arr[i])
+            return false;
+    }
+    return true;
+}
+
+void marker(threadArgs* args){
+    std::unique_lock<std::mutex> startlk(startMutex);
+    startCV.wait(startlk, []()->bool{return ready;});
+    startlk.unlock();
+    std::cout << "Thread " << args->num << " has been started.\n";
+
+    int count = 0;
+    while (true) {
+        std::unique_lock<std::mutex> lk(markerMutex);
+        int i = getRandomNumber(args->n);
+
+        if (args->arr[i] != 0) {
+            std::cout << "Thread №" << args-> num << ", marked " << count << "elements, " << " unable to mark №" << i << '\n';
+
+            threadsPaused[args->num - 1] = true;
+            stateChanged.notify_all();
+            lk.unlock();
+            std::unique_lock<std::mutex> actionLock(args->actionMutex);
+            
+            args->doAction.wait(actionLock, [=]()->bool{return args->actions[1] || args->actions[0];});
+
+            if(args->actions[1]){
+                for(int i = 0; i < args->n; ++i) {
+                    if (args->arr[i] == args->num) {
+                        args->arr[i] = 0;
+                    }
+                }
+
+                std::cout << "Thread" << args->num << "has been terminated.";
+                break;
+            }
+
+            args->actions[0] = false;
+        } else{
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+            ++count;
+            args->arr[i] = args->num;
+            lk.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+    }
 }
 
 int main() {
